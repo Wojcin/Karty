85434582dcb8bba16f37e63adecdd5ef7e02026a
Wojcin <225943@student.pwr.edu.pl>
2016-04-08 12:22:32 +0200

    Git init

---
+++ card.cpp
@@ -0,0 +1,22 @@
+#include "card.h"
+int Card::counter=0;
+Card::Card()
+
+{
+figure= counter %13;
+
+colour= counter++ /13;
+}		
+	result Card::compare(Card*)
+	{
+		return win;
+
+	}
+	string Card::description()
+	{
+		static char colours[]="KPTR";
+		static char figures[]="234567890JQKA";
+		return string (1,colours[colour]) + string (1, figures[figure]);
+
+	}
+
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include "card.h"
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	Card cards[52];
+	
+}
